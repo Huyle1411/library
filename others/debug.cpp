88854ca39f1df0f846d578bce54ab62531f5e011
Huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2021-09-16 22:50:48 +0700

    change debug

--- others/debug.cpp
+++ others/debug.cpp
@@ -1,9 +1,34 @@
 namespace std {
-std::string to_string(std::string s) { return '"' + s + '"'; }
+template <typename A, typename B>
+std::string to_string(std::pair<A, B> p);
+template <typename A, typename B, typename C>
+std::string to_string(std::tuple<A, B, C> p);
+template <typename A, typename B, typename C, typename D>
+std::string to_string(std::tuple<A, B, C, D> p);
+std::string to_string(const std::string &s) { return '"' + s + '"'; }
 std::string to_string(const char *s) { return to_string((std::string)s); }
 std::string to_string(bool b) { return (b ? "true" : "false"); }
-template <typename A, typename B>
-std::string to_string(std::pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+std::string to_string(std::vector<bool> v) {
+  bool first = true;
+  std::string res = "{";
+  for (int i = 0; i < static_cast<int>(v.size()); i++) {
+    if (!first) {
+      res += ", ";
+    }
+    first = false;
+    res += to_string(v[i]);
+  }
+  res += "}";
+  return res;
+}
+template <size_t N>
+std::string to_string(std::bitset<N> v) {
+  std::string res = "";
+  for (size_t i = 0; i < N; i++) {
+    res += static_cast<char>('0' + v[i]);
+  }
+  return res;
+}
 template <typename A>
 std::string to_string(A v) {
   bool first = true;
@@ -13,11 +38,17 @@ std::string to_string(A v) {
       res += ", ";
     }
     first = false;
-    res += std::to_string(x);
+    res += to_string(x);
   }
   res += "}";
   return res;
 }
+template <typename A, typename B>
+std::string to_string(std::pair<A, B> p) { return "(" + to_string(p.first) + ", " + to_string(p.second) + ")"; }
+template <typename A, typename B, typename C>
+std::string to_string(std::tuple<A, B, C> p) { return "(" + to_string(std::get<0>(p)) + ", " + to_string(std::get<1>(p)) + ", " + to_string(std::get<2>(p)) + ")"; }
+template <typename A, typename B, typename C, typename D>
+std::string to_string(std::tuple<A, B, C, D> p) { return "(" + to_string(std::get<0>(p)) + ", " + to_string(std::get<1>(p)) + ", " + to_string(std::get<2>(p)) + ", " + to_string(std::get<3>(p)) + ")"; }
 }  // namespace std
 void debug_out() { std::cerr << std::endl; }
 template <typename Head, typename... Tail>

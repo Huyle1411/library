8db3e701afcf447355dc45e0fe1dc06563589ed3
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-03-09 09:21:57 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 int main() {
-  cin.tie(0)->sync_with_stdio(0); 
-  cin.exceptions(cin.failbit);
+  std::cin.tie(0)->sync_with_stdio(0); 
+  std::cin.exceptions(cin.failbit);
   int T;
   std::cin >> T;
   while (T--) {

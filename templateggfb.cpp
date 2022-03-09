a2e5ce87a86ca254f6564865d0428b0235a71719
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-03-09 09:21:37 +0700

    Update templateggfb.cpp

--- templateggfb.cpp
+++ templateggfb.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 int main() {
-  std::ios::sync_with_stdio(false);
-  std::cin.tie(nullptr);
+  std::cin.tie(0)->sync_with_stdio(0); 
+  std::cin.exceptions(cin.failbit);
   int T;
   std::cin >> T;
   for (int tc = 1; tc <= T; ++tc) {

33f3d22fbe548e92874da25255d09d87dc5b6734
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-07-11 13:33:29 +0700

    update

--- template.cpp
+++ template.cpp
@@ -1,23 +1,16 @@
 #include <bits/stdc++.h>
+using i64 = long long;
 #ifdef DEBUG
 #include "debug.h"
 #else
 #define debug(...) 69
 #endif
-#define runcase ^-^
-using i64 = long long;
-
-void solve() {
-}
 
 int32_t main() {
   std::cin.tie(0)->sync_with_stdio(0);
   std::cin.exceptions(std::cin.failbit);
-  int T = 1;
-#ifdef runcase
-  std::cin >> T;
-#endif
-  for (int tc = 0; tc < T; ++tc) {
-    solve();
+  int tt = 1;
+  std::cin >> tt;
+  while (tt--) {
   }
 }

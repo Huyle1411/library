a7d1adf3519867e14d4e773735b6657c0df0b706
huyle1411 <44628783+Huyle1411@users.noreply.github.com>
2022-09-30 16:48:54 +0700

    Update binpow.cpp

--- algebra/binpow.cpp
+++ algebra/binpow.cpp
@@ -1,6 +1,5 @@
-template <typename T>
-T binpow(T base, T expon) {
-  T res = 1;
+i64 binpow(i64 base, i64 expon) {
+  i64 res = 1;
   while (expon > 0) {
     if (expon & 1)
       res *= base;
@@ -10,10 +9,9 @@ T binpow(T base, T expon) {
   return res;
 }
 
-template <typename T>
-T binpow(T base, T expon, T mod) {
+i64 binpow(i64 base, i64 expon, i64 mod) {
   base %= mod;
-  T res = 1;
+  i64 res = 1;
   while (expon > 0) {
     if (expon & 1)
       res = res * base % mod;
